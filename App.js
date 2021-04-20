2cc8e20e65bc62a3c6f28baf15d4be9f0eab5368
tatiana-passarella <tatiana.passarella@gmail.com>
2021-04-20 14:40:25 +0200

    getMetricMetaInfo function and AddEntry component

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import AddEntry from './components/AddEntry'
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello android world!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text>Hello android world!!!</Text>
+        <AntDesign name="camera" size={24} style={{ fontSize: '40px', color: 'red' }} />
+      </View>
+      <View style={styles.container}>
+        <AddEntry />
+      </View>
+    </>
   );
 }
 
