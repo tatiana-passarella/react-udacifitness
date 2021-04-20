ef579d8925ab348d757d829ac5d373e7b44ba9a7
tatiana-passarella <tatiana.passarella@gmail.com>
2021-04-20 15:25:37 +0200

    UdaciSlider and UdaciSteppers components, render minimal UI with icons and slider or stepper

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
 import AddEntry from './components/AddEntry'
 
 
 export default function App() {
   return (
-    <>
-      <View style={styles.container}>
-        <Text>Hello android world!!!</Text>
-        <AntDesign name="camera" size={24} style={{ fontSize: '40px', color: 'red' }} />
-      </View>
       <View style={styles.container}>
         <AddEntry />
       </View>
-    </>
   );
 }
 
--- utils/colors.js
+++ utils/colors.js
@@ -1,7 +1,7 @@
 export const purple = '#292477'
 export const gray = '#757575'
 export const white = '#fff'
-export const red = '#b71845'
+export const red = '#CC0000'
 export const orange = '#f26f28'
 export const blue = '#4e4cb8'
 export const lightPurp = '#7c53c3'
