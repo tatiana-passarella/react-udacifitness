7840864b286347cd37b5173c9e7bd6d526db18d1
tatiana-passarella <tatiana.passarella@gmail.com>
2021-04-22 16:27:55 +0200

    Button example to reset state

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
 
+function SubmitBtn ({ onPress }) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={styles.btn}>
+        <Text>SUBMIT</Text>
+    </TouchableOpacity>
+  )
+}
+
 export default class AddEntry extends Component {
   state = {
     run: 0,
-    bike: 0,
-    swim: 0,
+    bike: 230,
+    swim: 8,
     sleep: 0,
-    eat: 0,
+    eat: 5,
+  }
+  submit = () => {
+    const key = timeToString()
+    const entry = this.state
+
+    // Update Redux
+
+    this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }))
+
+    // Navigate to home
+
+    // Save to "DB"
+
+    // Clear local notification
   }
   increment = (metric) => {
     const { max, step } = getMetricMetaInfo(metric)
@@ -46,6 +70,7 @@ export default class AddEntry extends Component {
     return (
       <View>
         <DateHeader date={(new Date()).toLocaleDateString()}/>
+        <Text>{JSON.stringify(this.state)}</Text>
         {Object.keys(metaInfo).map((key) => {
           const { getIcon, type, ...rest } = metaInfo[key]
           const value = this.state[key]
@@ -68,7 +93,20 @@ export default class AddEntry extends Component {
             </View>
           )
         })}
+        <SubmitBtn onPress={this.submit} />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  btn: {
+    backgroundColor: '#FF0000',
+    padding: 10,
+    paddingLeft: 50,
+    paddingRight: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+});
