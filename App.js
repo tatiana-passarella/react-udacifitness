5ec0aabe03a7ece14787b1ed8cf37ea53f3fc9eb
tatiana-passarella <tatiana.passarella@gmail.com>
2021-05-05 15:48:42 +0200

    redux and react-redux added

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AddEntry from './components/AddEntry'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 
 
 export default function App() {
   return (
+    <Provider store={createStore(reducer)}>
       <View style={styles.container}>
         <AddEntry />
       </View>
+    </Provider>
   );
 }
 
